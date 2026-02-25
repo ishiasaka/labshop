f5e23d6ffdcce0566ac0ad49ef861d25969f2214
Sila Sonpee <hirasawa_au@yahoo.co.jp>
2026-02-25 19:34:19 +0900

    correct import

--- api/routes/ic_cards.py
+++ api/routes/ic_cards.py
@@ -3,8 +3,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from schema import ICCardCreate, UserStatus
 from datetime import datetime, timezone
 from models import AdminLog, ICCard, Purchase, User, Shelf, SystemSetting
-from ws.connection_manager import ws_connection_manager
-from ws.ws_schema import WSSchema
+from services.ws import ConnectionManager, WSSchema
 
 from schema import CardRegistrationRequest, ICCardStatus, PurchaseStatus, ScanRequest
 from services.auth import get_current_admin, TokenData
